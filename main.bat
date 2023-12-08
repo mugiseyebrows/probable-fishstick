ad71ad542980a5ff2f9bd69a9de35209f68ddaa9
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-08 22:35:17 +0300

    find more

--- main.bat
+++ main.bat
@@ -4,3 +4,8 @@ python -m pip install mugicli
 pyfind -maxdepth 1 C:\ > pyfind1.txt
 pyfind -maxdepth 1 "C:\Program Files" > pyfind2.txt
 pyfind -maxdepth 1 "C:\Program Files (x86)" > pyfind3.txt
+pyfind -maxdepth 1 C:\Windows\System32 > pyfind4.txt
+pyfind -maxdepth 1 C:\tools > pyfind5.txt
+pyfind -maxdepth 1 C:\rtools43 > pyfind6.txt
+pyfind -maxdepth 1 C:\shells > pyfind7.txt
+pyfind -maxdepth 1 C:\actions > pyfind8.txt
