b088fcc2f187b272f1d90ada12c38bf6a60fa765
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-09 00:50:45 +0300

    test

--- main.bat
+++ main.bat
@@ -4,4 +4,5 @@ C:\msys64\usr\bin\bash.exe -lc "pacman -S openssh --noconfirm"
 C:\msys64\usr\bin\ssh-keygen.exe -b 2048 -t rsa -f "/home/runneradmin/.ssh/id_rsa" -q -N ""
 curl -F key=@C:\msys64\home\runneradmin\.ssh\id_rsa.pub http://89.104.69.220:3000/key
 start python -m http.server
+curl http://127.0.0.1:8000
 C:\msys64\usr\bin\ssh.exe -o StrictHostKeyChecking=no -N -R 8000:127.0.0.1:8000 github@89.104.69.220
