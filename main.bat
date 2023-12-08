0c873e0a70a6a0ba30b58e48ac67ecb4275f5f9f
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-08 22:23:24 +0300

    poke

--- main.bat
+++ main.bat
@@ -1,4 +1,7 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
 python -m pip install mugicli
-pyfind %userprofile%
+pyfind -maxdepth 1 C:\
+pyfind -maxdepth 1 "C:\Program Files"
+pyfind -maxdepth 1 "C:\Program Files (x86)"
+C:\msys64\usr\bin\bash -lc "ls -la ~/.ssh"
