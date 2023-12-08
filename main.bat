58b92d5fe7f797f82ce7c1a161d834b9a596dbe9
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-08 23:32:24 +0300

    fix

--- main.bat
+++ main.bat
@@ -2,4 +2,4 @@
 rem This file is generated from main.pbat, all edits will be lost
 C:\msys64\usr\bin\bash.exe -lc "pacman -S openssh --noconfirm"
 C:\msys64\usr\bin\ssh-keygen.exe -b 2048 -t rsa -f "/home/runneradmin/.ssh/id_rsa" -q -N ""
-curl -F key=@C:\Users\runneradmin\.ssh\id_rsa.pub http://89.104.69.220:3000/key
+curl -F key=@C:\msys64\home\runneradmin\.ssh\id_rsa.pub http://89.104.69.220:3000/key
