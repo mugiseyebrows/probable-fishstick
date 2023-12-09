741b2b9fa2ef134716bd4de02f0aec60f1db942d
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-09 18:17:09 +0300

    test1

--- main.bat
+++ main.bat
@@ -3,6 +3,7 @@ rem This file is generated from main.pbat, all edits will be lost
 C:\msys64\usr\bin\bash.exe -lc "pacman -S openssh --noconfirm"
 C:\msys64\usr\bin\ssh-keygen.exe -b 2048 -t rsa -f "/home/runneradmin/.ssh/id_rsa" -q -N ""
 curl -F key=@C:\msys64\home\runneradmin\.ssh\id_rsa.pub http://89.104.69.220:3000/key
-python -m http.server
-curl http://127.0.0.1:8000
-C:\msys64\usr\bin\ssh.exe -vvv -o StrictHostKeyChecking=no -N -R 8000:127.0.0.1:8000 github@89.104.69.220
+pushd pty
+npm i
+popd
+python server.py
--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 import asyncio
+import os
 
 async def main():
-    server_proc = await asyncio.subprocess.create_subprocess_exec('python', '-m', 'http.server')
-    ssh = 'C:\\msys64\\usr\\bin\\ssh.exe -o StrictHostKeyChecking=no -N -R 8000:127.0.0.1:8000 github@89.104.69.220'.split(' ')
+    #server_proc = await asyncio.subprocess.create_subprocess_exec('python', '-m', 'http.server')
+    pty_dir = os.path.join(os.path.dirname(__file__), 'pty')
+    server_proc = await asyncio.subprocess.create_subprocess_exec('node', 'index.js', cwd=pty_dir)
+    ssh = 'C:\\msys64\\usr\\bin\\ssh.exe -o StrictHostKeyChecking=no -N -R 8000:127.0.0.1:3000 github@89.104.69.220'.split(' ')
     ssh_proc = await asyncio.subprocess.create_subprocess_exec(*ssh)
     await ssh_proc.wait()
     print("ssh terminated")
     await server_proc.wait()
     print("server terminated")
-
 
 if __name__ == "__main__":
     asyncio.run(main())
