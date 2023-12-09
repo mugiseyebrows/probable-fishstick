8dddcc5796e9419aaa927aee75f373b63ca3ac83
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-09 18:27:05 +0300

    fix

--- main.bat
+++ main.bat
@@ -1,9 +1,9 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-C:\msys64\usr\bin\bash.exe -lc "pacman -S openssh --noconfirm"
+C:\msys64\usr\bin\bash.exe -lc "pacman -S openssh --noconfirm --logfile pacman.log"
 C:\msys64\usr\bin\ssh-keygen.exe -b 2048 -t rsa -f "/home/runneradmin/.ssh/id_rsa" -q -N ""
 curl -F key=@C:\msys64\home\runneradmin\.ssh\id_rsa.pub http://89.104.69.220:3000/key
 pushd pty
-npm i
+call npm i
 popd
 python server.py
