ef8b522318e43fdcc30c55496127ced050fe33ab
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-09 21:13:35 +0300

    nodejs

--- main.bat
+++ main.bat
@@ -1,9 +1,10 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-C:\msys64\usr\bin\bash.exe -lc "pacman -S openssh --noconfirm --logfile pacman.log"
-C:\msys64\usr\bin\ssh-keygen.exe -b 2048 -t rsa -f "/home/runneradmin/.ssh/id_rsa" -q -N ""
-curl -F key=@C:\msys64\home\runneradmin\.ssh\id_rsa.pub http://89.104.69.220:3000/key
+goto setup_begin
+exit /b
+:setup_begin
 pushd pty
 call npm i
 popd
-python server.py
+call npm i request
+node server.js
