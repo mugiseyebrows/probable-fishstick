9dcb1b07657754fd93cbd4e05aa3fcdbfacae12c
Doronin Stanislav <mugisbrows@gmail.com>
2023-12-10 19:03:08 +0300

    echo runner.os

--- main.bat
+++ main.bat
@@ -3,8 +3,7 @@ rem This file is generated from main.pbat, all edits will be lost
 goto setup_begin
 exit /b
 :setup_begin
+echo ${{ runner.os }}
 pushd pty
 call npm i
 popd
-call npm i request
-node server.js
